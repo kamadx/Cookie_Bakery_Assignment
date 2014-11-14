a77217a4fadcc933f0d15151110203eb665a4bdf
kamadx <horjan13@student.westerdals.no>
2014-11-14 15:17:11 +0100

    Skrev litt i Customer.cs

--- Cookie_Bakery/Customer.cs
+++ Cookie_Bakery/Customer.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Cookie_Bakery
 {
 	class Customer
 	{
+        string name;
+        List<int> cookies;
+        Stopwatch timer;
+
+
+        public Customer(string name) 
+        {
+            this.name = name;
+        }
+
+        public void checkForCookie()
+        {
+            timer.Start();
+            int cookieNumber;
+            
+        }
 	}
 }
