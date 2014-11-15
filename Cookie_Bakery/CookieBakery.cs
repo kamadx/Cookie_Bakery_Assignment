a53f2beac969cdba9f00e705b625dca3bbc53b15
kamadx <horjan13@student.westerdals.no>
2014-11-15 19:57:19 +0100

    Cleanup + A little bit of commenting

--- Cookie_Bakery/CookieBakery.cs
+++ Cookie_Bakery/CookieBakery.cs
@@ -2,60 +2,55 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 
-
-
-
-
 namespace Cookie_Bakery
 {
-	class CookieBakery
-	{
-		public List<int> cookies;
-		private Stopwatch timer;
-		private int count;
+    class CookieBakery
+    {
+        public List<int> cookies;
+        private Stopwatch timer;
+        private int count;
 
-		public CookieBakery() 
-		{
-			cookies = new List<int>();
-			timer = new Stopwatch();
-			count = 0;
-		}
+        public CookieBakery()
+        {
+            cookies = new List<int>();
+            timer = new Stopwatch();
+            count = 0;
+        }
 
-		public void Start()
-		{
+        public void start()
+        {
 
-			timer.Start();
-			while (count <= 70)
-			{
-				if (timer.ElapsedMilliseconds >= 700)
-				{
-					BakeCookie();
-					timer.Restart();
-				}
-			}
+            timer.Start();
+            while (count <= 5)
+            {
+                if (timer.ElapsedMilliseconds >= 700)
+                {
+                    bakeCookie();
+                    timer.Restart();
+                }
+            }
             Console.WriteLine("\nHurry, this is the last Cookie!\n");
-		}
+        }
 
-		/// <summary>
-		/// Bake a cookie.
-		/// </summary>
-		/// <remarks>Lets see, how do these show up?</remarks>
-		private void BakeCookie()
-		{
-			cookies.Add(count);
-			Console.WriteLine("Baked cookie #{0}.", count);
-			count++;
-		}
+        /// <summary>
+        /// Bakes a cookie and adds it to the list of cookies.
+        /// </summary>
+        private void bakeCookie()
+        {
+            cookies.Add(count);
+            Console.WriteLine("Baked cookie #{0}.", count);
+            count++;
+        }
 
-		/// <summary>
-		/// sells a cookie to a customer.
-		/// </summary>
-		/// <param name="customer"></param>
-		public void sellCookieTo(Customer customer)
-		{
-			customer.cookies.Add(cookies[0]);
-			cookies.RemoveAt(0);
-		}
+        /// <summary>
+        /// Sells cookie to Customer
+        /// and removes the last made cookie in the list of cookies
+        /// </summary>
+        public void sellCookieTo(Customer customer)
+        {
+            customer.cookies.Add(cookies[0]);
+            cookies.RemoveAt(0);
+        }
 
-	}
+    }
 }
--- Cookie_Bakery/Customer.cs
+++ Cookie_Bakery/Customer.cs
@@ -1,41 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Threading;
 
 namespace Cookie_Bakery
 {
-	class Customer
-	{
-		string name;
-		public List<int> cookies = new List<int>();
-		Stopwatch timer = new Stopwatch();
-		CookieBakery bakery;
+    class Customer
+    {
+        string name;
+        public List<int> cookies = new List<int>();
+        Stopwatch timer = new Stopwatch();
+        CookieBakery bakery;
 
-		public Customer(string name, CookieBakery bakery)
-		{
-			this.name = name;
-			this.bakery = bakery;
-		}
+        public Customer(string name, CookieBakery bakery)
+        {
+            this.name = name;
+            this.bakery = bakery;
+        }
 
-		public void buyCookies()
-		{
-			timer.Start();
-			while (true)
-			{
-				if (timer.ElapsedMilliseconds >= 1000)
-				{
-					lock (bakery)
-					{
-						if (bakery.cookies.Count >= 1)
-						{
-							bakery.sellCookieTo(this);
-							Console.WriteLine("\t" + name + " bought cookie #{0}.", cookies[cookies.Count - 1]);
-						}
-					}
-					timer.Restart();
-				}
-			}
-		}
-	}
+        /// <summary>
+        /// Buys a cookie
+        /// </summary>
+        public void buyCookies()
+        {
+            timer.Start();
+            while (true)
+            {
+                if (timer.ElapsedMilliseconds >= 1000)
+                {
+                    lock (bakery)
+                    {
+                        if (bakery.cookies.Count >= 1)
+                        {
+                            bakery.sellCookieTo(this);
+                            Console.WriteLine("\t\t\t" + name + " bought cookie #{0}.", cookies[cookies.Count - 1]);
+                        }
+                    }
+                    timer.Restart();
+                }
+            }
+        }
+    }
 }
